0f3c052844259519d9eb2251434f4a952bca2c7e
jward <jacob.ward2009@gmail.com>
2026-09-03 16:06:45 -0400

    Add player rotation

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def main():
     while True:
         log_state()
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
